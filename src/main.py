b62c3b3e17c336945748e9c1d693c2b902074c6c
Colin Gorrie <colin@colingorrie.com>
2019-02-10 12:29:31 -0500

    initial commit

---
+++ src/main.py
@@ -0,0 +1,21 @@
+class Agent:
+    def __init__(self):
+        pass
+
+
+class Cell:
+    def __init__(self):
+        pass
+
+
+class World:
+    def __init__(self):
+        pass
+
+
+def main():
+    print("Hello world")
+
+
+if __name__ == "__main__":
+    main()
